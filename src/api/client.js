2c0f88a633b640d0c69164b062f0609b56e46b78
kamalafidele <kamalafizzet45@gmail.com>
2024-02-23 00:13:21 +0200

    added url for API

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import { create } from 'apisauce';
-// https://url-shortener.onrender.com/api/v1
+
 const apiClient = create({
-    baseURL: 'http://localhost:8080/api/v1',
+    baseURL: 'https://url-shortener-be-k0sr.onrender.com/api/v1',
     timeout: 1000000,
 });
 
